b9ba5fa5cecbbccbe4153f590700a87a3ec10b7a
generalpask <nils.korsfeldt@gmail.com>
2021-05-01 19:58:57 +0200

    Added solarized color theme and set font

--- config.def.h
+++ config.def.h
@@ -5,17 +5,26 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "LiterationMono Nerd Font:size=12" };
+static const char dmenufont[]       = "LiterationMono Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_base03[]      = "#002b36";
+static const char col_base02[]      = "#073642";
+static const char col_base0[]       = "#839496";
+static const char col_base1[]       = "#93a1a1";
+
+static const char col_oldcyan[]     = "#005577";
+
+static const char col_yellow[]      = "#b58900";
+static const char col_blue[]        = "#268bd2";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_base0, col_base03, col_gray2 },
+	[SchemeSel]  = { col_base1, col_base02,  col_blue  },
 };
 
 /* tagging */
@@ -57,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_base03, "-nf", col_base0, "-sb", col_base02, "-sf", col_base1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *flameshotgui[] = { "flameshot", "gui", NULL };
 
