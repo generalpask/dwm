f4471cfdaff062e40bab71f8302eeae93b84b9c1
generalpask <nils.korsfeldt@gmail.com>
2021-05-02 22:04:00 +0200

    Added anybar patch

--- config.def.h
+++ config.def.h
@@ -7,6 +7,9 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "LiterationMono Nerd Font:size=12" };
 static const char dmenufont[]       = "LiterationMono Nerd Font:size=12";
+static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
+static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
