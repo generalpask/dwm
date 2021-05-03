7d3a2d94123b9749a91b5d6ef6edb19a363de528
generalpask <nils.korsfeldt@gmail.com>
2021-05-03 03:10:12 +0200

    Use alt bar (polybar)

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "LiterationMono Nerd Font:size=12" };
 static const char dmenufont[]       = "LiterationMono Nerd Font:size=12";
-static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char col_gray1[]       = "#222222";
