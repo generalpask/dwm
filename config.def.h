5cef818bef80f621afa1d4ffdaab75f85fdfc194
generalpask <nils.korsfeldt@gmail.com>
2022-01-24 13:12:09 +0100

    Temporarily disable polybar as the dwm module is broken

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "LiterationMono Nerd Font:size=12" };
 static const char dmenufont[]       = "Fira Code:size=12";
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *altbarcmd        = "$XDG_CONFIG_HOME/polybar/launch.sh"; /* Alternate bar launch command */
 static const char col_gray1[]       = "#222222";
